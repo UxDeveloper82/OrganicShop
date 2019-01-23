307a7d7946facf985e71754f32290f20625a1f5f
electronicsmf <electronicsmf@aol.com>
2019-01-23 08:43:08 -0500

    Firebase Set up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: 'AIzaSyAesHDSpf8LEEnlObtB5h4QXlAq3agS01w',
+  authDomain: 'oshop-d90ae.firebaseapp.com',
+  databaseURL: 'https://oshop-d90ae.firebaseio.com',
+  projectId: 'oshop-d90ae',
+  storageBucket: 'oshop-d90ae.appspot.com',
+  messagingSenderId: '933303167167'}
 };
 
 /*
